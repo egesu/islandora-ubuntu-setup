ae2d97036abd4f260aeb60d36767f04f48b5e257
Ege <alikemal@riseup.net>
2016-09-05 16:18:09 +0300

    Do not install drush if installed

--- 004-d5-drush.sh
+++ 004-d5-drush.sh
@@ -1,6 +1,14 @@
 #!/bin/bash
 
+DRUSH_VERSION="$(drush --version | sed -n 1p)"
+
 # Specify drush commit due to issues with newer drush versions and automated tests used by QA
-cd /opt
-git clone https://github.com/drush-ops/drush.git && cd drush && git checkout b9e6c8c00da0fbf1227869cdf915b0c6cea466cc
-ln -s /opt/drush/drush /usr/bin/drush
+
+if [ "$DRUSH_VERSION" != " Drush Version   :  6.3.0" ]; then
+    echo "Installing drush"
+    cd /opt
+    git clone https://github.com/drush-ops/drush.git && cd drush && git checkout b9e6c8c00da0fbf1227869cdf915b0c6cea466cc
+    ln -s /opt/drush/drush /usr/bin/drush
+else
+    echo "Drush already installed"
+fi
