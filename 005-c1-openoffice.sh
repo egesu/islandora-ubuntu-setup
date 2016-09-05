02764c6fb4d8a3e459342b01df6b12e7d9600498
Ege <alikemal@riseup.net>
2016-09-05 17:33:13 +0300

    Fixed adding virtualhost

--- 005-c1-openoffice.sh
+++ 005-c1-openoffice.sh
@@ -9,8 +9,7 @@ service openoffice start
 #If Service doesn't start that is because apache has no shell
 $SCHEDULE_OPENOFFICE_SERVICE_COMMAND
 
-echo -e "check process openoffice\n matching \"/usr/lib/libreoffice/program/soffice.bin\"\n start program = \"/etc/init.d/openoffice start\"\n stop program = \"/etc/init.d/openoffice stop\"\n if failed host 127.0.0.1 port 8100 then restart\n if 5 restarts within 5 cycles then timeout"
-$MONIT_CONF_DIR/openoffice.conf
+echo -e "check process openoffice\n matching \"/usr/lib/libreoffice/program/soffice.bin\"\n start program = \"/etc/init.d/openoffice start\"\n stop program = \"/etc/init.d/openoffice stop\"\n if failed host 127.0.0.1 port 8100 then restart\n if 5 restarts within 5 cycles then timeout" > $MONIT_CONF_DIR/openoffice.conf
 sed -i 's|# set httpd| set httpd|g' $MONIT_CONFIG_FILE
 sed -i 's|#    use address|    use address|g' $MONIT_CONFIG_FILE
 sed -i 's|#    allow localhost|    allow localhost|g' $MONIT_CONFIG_FILE
