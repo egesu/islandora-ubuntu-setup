73c57bd9f4af64b01d13f36e158ef0b9c8adcd48
Ege <alikemal@riseup.net>
2016-09-05 15:11:08 +0300

    Do not install ghostscript if already installed

--- 004-d1-ghostscript.sh
+++ 004-d1-ghostscript.sh
@@ -2,16 +2,20 @@
 
 echo "Installing Ghostscript v9.05"
 
-if [ ! -d ghostscript-9.05 ]; then
-    echo "Downloading ghostscript"
-    wget http://downloads.ghostscript.com/public/old-gs-releases/ghostscript-9.05.tar.gz
-    tar xvzf ghostscript-9.05.tar.gz
+if [ "$(gs --version)" != "9.05" ]; then
+    if [ ! -d ghostscript-9.05 ]; then
+        echo "Downloading ghostscript"
+        wget http://downloads.ghostscript.com/public/old-gs-releases/ghostscript-9.05.tar.gz
+        tar xvzf ghostscript-9.05.tar.gz
+    else
+        echo "Already downloaded ghostscript"
+    fi
+
+    cd ghostscript-9.05
+    ./configure
+    make && make install
+else
+    echo "Ghostscript is already installed"
 fi
 
-cd ghostscript-9.05
-./configure
-make && make install
-
-# should return result
-gs --version
 cd ..
