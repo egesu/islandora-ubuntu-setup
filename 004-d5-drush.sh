203a0634977221c4269a7bceb06f85fd3b88adac
Ege <alikemal@riseup.net>
2016-09-05 16:28:16 +0300

    Fixed checking version of drush

--- 004-d5-drush.sh
+++ 004-d5-drush.sh
@@ -4,7 +4,7 @@ DRUSH_VERSION="$(drush --version | sed -n 1p)"
 
 # Specify drush commit due to issues with newer drush versions and automated tests used by QA
 
-if [ "$DRUSH_VERSION" != " Drush Version   :  6.3.0" ]; then
+if [[ $DRUSH_VERSION != *"6.3.0"* ]]; then
     echo "Installing drush"
     cd /opt
     git clone https://github.com/drush-ops/drush.git && cd drush && git checkout b9e6c8c00da0fbf1227869cdf915b0c6cea466cc
