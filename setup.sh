07c56f31a180dcec741a2e7411cebe2e80ab465f
Ege <alikemal@riseup.net>
2016-09-05 16:18:26 +0300

    Do not install ffmpeg

--- setup.sh
+++ setup.sh
@@ -13,10 +13,10 @@ echo "Running 004-d1"
 ./004-d1-ghostscript.sh &> log/004-d1.log
 
 echo "Running 004-d2"
-./004-d2-ffmpeg.sh &> log/004-d2.log
+# ./004-d2-ffmpeg.sh &> log/004-d2.log
 
 echo "Running 004-d3"
-./004-d3-ffmpeg2theora.sh &> log/004-d3.log
+# ./004-d3-ffmpeg2theora.sh &> log/004-d3.log
 
 echo "Running 004-d4"
 ./004-d4-fits.sh &> log/004-d4.log
