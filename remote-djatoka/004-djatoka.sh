25954a3f54e4e34fe08a06cb0d1ac086e7aede2b
Ege <alikemal@riseup.net>
2016-09-07 12:55:00 +0300

    Remote Djatoka installation fixes and setenv.sh example

--- remote-djatoka/004-djatoka.sh
+++ remote-djatoka/004-djatoka.sh
@@ -4,6 +4,8 @@ source ../001.environment.sh
 
 KDU_EXPAND_VERSION="$(kdu_expand -v)"
 KDU_COMPRESS_VERSION="$(kdu_compress -v)"
+CATALINA_HOME=/var/lib/tomcat7
+TOMCAT_USER=tomcat7
 
 echo "Installing djakota"
 cd /opt
@@ -17,9 +19,8 @@ ln -s /opt/adore-djatoka/bin/Linux-x86-64/kdu_compress /usr/bin/kdu_compress
 echo "/opt/adore-djatoka/lib/Linux-x86-64" > /etc/ld.so.conf.d/kakadu.conf
 ldconfig
 
-exit
-
-cp /opt/adore-djatoka/dist/adore-djatoka.war $CATALINA_HOME/webapps && unzip -o /opt/adore-djatoka/dist/adore-djatoka.war -d $CATALINA_HOME/webapps/adore-djatoka
+cp /opt/adore-djatoka/dist/adore-djatoka.war $CATALINA_HOME/webapps
+unzip -o /opt/adore-djatoka/dist/adore-djatoka.war -d $CATALINA_HOME/webapps/adore-djatoka
 
 cd ~
 
@@ -29,10 +30,12 @@ cd islandora_log_config
 
 cp islandora_logrotate /etc/logrotate.d/
 
-cp log4j.xml /usr/local/fedora/tomcat/webapps/fedoragsearch/WEB-INF/classes/log4j.xml
+# cp log4j.xml /usr/local/fedora/tomcat/webapps/fedoragsearch/WEB-INF/classes/log4j.xml
 
-cp logging.properties /usr/local/fedora/tomcat/conf/logging.properties
+cp logging.properties /etc/tomcat7/logging.properties
 
-cp log4j.properties /usr/local/fedora/tomcat/webapps/adore-djatoka/WEB-INF/classes/log4j.properties
+cp log4j.properties $CATALINA_HOME/webapps/adore-djatoka/WEB-INF/classes/log4j.properties
 
-chown -R $FEDORA_USER:$FEDORA_USER $FEDORA_HOME
+cp setenv.sh.example /usr/share/tomcat7/bin/setenv.sh
+
+chown -R $TOMCAT_USER:$TOMCAT_USER $CATALINA_HOME
