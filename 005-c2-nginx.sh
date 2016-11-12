cc56e82b800af49d00eaa93690001ea7c1404f7b
Ege <alikemal@riseup.net>
2016-11-12 12:45:07 +0300

    Added nginx configuration

--- 005-c2-nginx.sh
+++ 005-c2-nginx.sh
@@ -3,11 +3,8 @@
 source 001-environment.sh
 
 # Write our apache config to sites-available
-cat apache.conf > /etc/apache2/sites-available/000-default.conf
-
-a2enmod rewrite
-a2enmod proxy
-a2enmod proxy_http
+cat nginx.conf > /etc/nginx/sites-available/islandora.conf
+ln -T /etc/nginx/sites-available/islandora.conf /etc/nginx/sites-enabled/islandora.conf
 
 pecl install uploadprogress
 
@@ -17,3 +14,6 @@ sed -i "s|post_max_size = 8M|post_max_size = 2048M|g" /etc/php5/fpm/php.ini
 sed -i "s|upload_max_filesize = 2M|upload_max_filesize = 2048M|g" /etc/php5/fpm/php.ini
 
 echo "apc.shm_size = 64M" >> $APC_CONFIG_FILE
+
+service nginx restart
+service php5-fpm restart
