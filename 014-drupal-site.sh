1bd7fa5824e74b2a798e8cbd1e2d88d1e6ce5243
Ege <alikemal@riseup.net>
2016-11-12 12:31:54 +0300

    Usage  from environment

--- 014-drupal-site.sh
+++ 014-drupal-site.sh
@@ -6,7 +6,7 @@ chown -R $APACHE_USER:$APACHE_USER /var/www/drupal7
 
 cd /var/www/drupal7
 
-drush -y site-install standard --account-name=$DRUPAL_ADMIN_USER --account-pass=$DRUPAL_ADMIN_PASS --db-url=mysql://$DRUPAL_DB_USER:$DRUPAL_DB_PASS@localhost/$DRUPAL_DB_NAME
+drush -y site-install standard --account-name=$DRUPAL_ADMIN_USER --account-pass=$DRUPAL_ADMIN_PASS --db-url=mysql://$DRUPAL_DB_USER:$DRUPAL_DB_PASS@$DB_SERVER/$DRUPAL_DB_NAME
 
 # Secure settings.php
 chmod 444 /var/www/drupal7/sites/default/settings.php
